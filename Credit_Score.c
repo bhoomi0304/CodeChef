f53068f5381260fc84481eb2634cedbdfa808633
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2022-12-28 16:07:36 +0530

    Create Credit_Score.c

---
+++ Credit_Score.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	    int x;
+	    scanf("%d",&x);
+	    if(x>=750){
+	        printf("yes\n");
+	    }
+	    else{
+	        printf("No\n");
+	    }
+	return 0;
+}
+
