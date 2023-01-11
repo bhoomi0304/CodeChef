012f5b61c7482435392a997043e7357e3f5ffbc1
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-01-11 22:29:42 +0530

    Create Sum_it.c

---
+++ Sum_it.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int a,b,c;
+	    scanf("%d %d %d",&a,&b,&c);
+	    int d=a+b;
+	    if(d==c)
+	        printf("YES\n");
+	    else
+	        printf("NO\n");
+	}
+	return 0;
+}
+
