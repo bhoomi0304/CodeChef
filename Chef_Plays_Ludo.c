adeea6f68a4c3489793857a8d3c0fabc85c2f86b
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-01-12 22:28:29 +0530

    Create Chef_Plays_Ludo.c

---
+++ Chef_Plays_Ludo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+      int x;
+      scanf("%d",&x);
+      if (x<=5)
+      printf("no\n");
+      else
+      printf("yes\n");
+	}
+	return 0;
+}
+
