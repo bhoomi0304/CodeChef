6cf9db97df0080389593f47b6791bf848ab25e55
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-01-15 21:26:52 +0530

    Create Biriyani_classes.c

---
+++ Biriyani_classes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T,a,b,count;
+	scanf("%d",&T);
+
+	while(T--){
+	       	scanf("%d%d",&a,&b);
+	        printf("%d\n",a*b);
+	}
+	return 0;
+}
+
