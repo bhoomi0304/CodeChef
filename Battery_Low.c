4730ef37b0ae99df11f73cc5ee10ef9f1f883683
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-03-18 22:48:57 +0530

    Create Battery_Low.c

---
+++ Battery_Low.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int x;
+	    scanf("%d",&x);
+	    if(x>15){
+	        printf("NO\n");
+	    }else
+	        printf("YES\n");
+	    
+	}
+	return 0;
+}
+
