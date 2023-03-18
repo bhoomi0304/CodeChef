2941de8f0522a4a86bf0928b223c1cc9a89e1c2c
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-03-18 22:11:35 +0530

    Create Just_one_more_episode.c

---
+++ Just_one_more_episode.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+// your code goes here
+int T;
+scanf("%d",&T);
+while(T--){
+   int n;
+   scanf("%d",&n);
+   if(n>24){
+       printf("YES\n");
+   }else
+       printf("NO\n");
+   
+}
+return 0;
+}
