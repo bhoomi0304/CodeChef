9619644303a88e1d67928d871771070082997fb6
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-03-18 22:10:46 +0530

    Create Tom_And_Jerry.c

---
+++ Tom_And_Jerry.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    if(y>x){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	    
+	}
+	return 0;
+}
+
