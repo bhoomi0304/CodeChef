c30317cc5bc857aa134d4ffaa4f0a4e20980a0a2
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-03-26 22:19:10 +0530

    Create counting_words.c

---
+++ counting_words.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int n,m;
+	    scanf("%d%d",&n,&m);
+	    printf("%d\n",n*m);
+	}
+	return 0;
+}
+
