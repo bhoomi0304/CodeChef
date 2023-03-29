d9c0117001d7748c062bb7b07031ff75ed81b58b
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-03-29 23:22:05 +0530

    Create Your_Name_IS_Mine.c

---
+++ Your_Name_IS_Mine.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+// your code goes here
+int t;
+scanf("%d",&t);
+while(t--)
+{
+   char m[25000],n[25000];
+   scanf("%s",m);
+   scanf("%s",n);
+   int l1=strlen(m);
+   int l2=strlen(n);
+   if(l1<l2){
+       int i=0,j=0;
+       while(n[j]!='\0'){
+           if(m[i]==n[j]){
+               i++;
+           }
+           j++;
+       }
+       if(m[i]=='\0'){
+               printf("YES\n");
+           }
+           else{
+               printf("NO\n");
+           }
+       }
+   
+       else
+       {
+           int i=0,j=0;
+           while(m[j]!='\0'){
+               if(n[i]==m[j]){
+                   i++;
+               }
+               j++;
+           }
+           if(n[i]=='\0'){
+               printf("YES\n");
+           }
+           else{
+               printf("NO\n");
+           }
+       }
+   }
+
+return 0;
+}
