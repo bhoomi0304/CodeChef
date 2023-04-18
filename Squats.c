9069add56818a70cc739c982f803ab3e9d46314f
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-04-18 22:32:54 +0530

    Create Squats.c

---
+++ Squats.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int x;
+	    scanf("%d",&x);
+	    printf("%d\n",x*15);
+	}
+	return 0;
+}
+
