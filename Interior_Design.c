7fbdd850b7028efa49de64168e35cec5ec4bf51e
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-04-27 17:36:00 +0530

    Create Interior_Design.c

---
+++ Interior_Design.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int x1,y1,x2,y2;
+    	scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+    	if(x1+y1<=x2+y2){
+    	    printf("%d\n",x1+y1);
+    	}
+    	else{
+    	    printf("%d\n",x2+y2);
+    	}
+	}
+	return 0;
+}
+
