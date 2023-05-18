5b95753403be387d7ad450fee0900337bb1c8c22
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-05-18 23:52:04 +0530

    Create FavouriteNumber.c

---
+++ FavouriteNumber.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int n;
+	    scanf("%d",&n);
+	    if(n%7==0 && n%2==0){
+	        printf("Alice\n");
+	    }
+	    else if(n%9==0 && n%2==1){
+	        printf("Bob\n");
+	    }
+	    else{
+	        printf("Charlie\n");
+	    }
+	}
+	return 0;
+}
+
