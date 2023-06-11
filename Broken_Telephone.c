6c7812d6ebf2d617b94ad379acd1d660be57adba
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-06-11 22:48:00 +0530

    Create Broken_Telephone.c

---
+++ Broken_Telephone.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int tc;
+	scanf("%d",&tc);
+	while(tc--){
+	    int n,count=0;
+	    scanf("%d",&n);
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	         scanf("%d",&a[i]);
+	    if(a[0]!=a[1])
+	        count++;
+	    if(a[n-1]!=a[n-2])
+	        count++;
+	    for(int i=1;i<n-1;i++)
+	    {
+	        if(a[i]!=a[i-1] || a[i]!=a[i+1])
+	            count++;
+	    }
+	    printf("%d\n",count);
+	}
+	return 0;
+}
+
