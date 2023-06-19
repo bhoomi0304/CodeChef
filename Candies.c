1f41a039c2c02fe434abea614d22e23490c93b6e
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-06-19 22:25:09 +0530

    Update Candies.c

--- Candies.c
+++ Candies.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 
 int main(void) {
-    int t;
-    scanf("%d",&t);
-    while(t--)
-    {int a;
-     scanf("%d",&a);
-     int c[2*a];
-     
-     for(int i=0; i<2*a; i++)
-      scanf("%d",&c[i]);
-      
-      int m=0;
-      
-      for(int i=0; i<2*a; i++)
-     {int n=0;
-         for(int j=0; j<2*a; j++)
-         if(c[i]==c[j])
-         n++;
-         if(n>m)
-         m=n;}
-         if(m>2)
-         printf("NO\n");
-         else
-         printf("Yes\n");
-   
-    }
-    
-   
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int n,flag=0;
+	    int freq[10]={0};
+	    scanf("%d",&n);
+	    int arr[n*2];
+	    for(int i=0;i<2*n;i++){
+	        scanf("%d",&arr[i]);
+	        freq[arr[i]]++;
+	    }
+	    for(int i=0;i<2*n;i++){
+	        int count=0;
+	       for(int j=i+1;j<2*n;j++){
+	           if(arr[i]==arr[j]){
+	               count++;
+	           }
+	       }
+	       if(count>1){
+	           flag=1;
+	           break;
+	       }
+	    }
+	    if(flag==1){
+	        printf("No\n");
+	    }
+	    else{
+	        printf("Yes\n");
+	    }
+	    
+	}
 	return 0;
 }
 
