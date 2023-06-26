207ed0a2f517a66ad271315d051ab1c3647b7e7f
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-06-26 21:43:36 +0530

    Create Donation_Drive.c

---
+++ Donation_Drive.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int p, k;
+        scanf("%d %d", &p, &k);
+        int jpk = p - k;
+        if (jpk <= 0) {
+            jpk = 0;
+        }
+        printf("%d\n", jpk);
+	}
+	return 0;
+}
+
