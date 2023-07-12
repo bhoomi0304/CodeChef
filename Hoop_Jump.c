eebbfefe42bf6e6b64a5a95dbb58cadb48606d52
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-07-12 22:03:27 +0530

    Create Hoop_Jump.c

---
+++ Hoop_Jump.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int a;
+	    scanf("%d",&a);
+	    printf("\n%d",(a/2)+1);
+	}
+	return 0;
+}
+
