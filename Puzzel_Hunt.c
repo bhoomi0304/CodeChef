b3ee90c18c4d1c3959ca47750afffff591ecfd55
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-07-28 23:09:05 +0530

    Create Puzzel_Hunt.c

---
+++ Puzzel_Hunt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	
+	    int n;
+	    scanf("%d",&n);
+	    (n>=6 && n<=8)?printf("YES\n"):printf("No\n");
+	    
+	
+	return 0;
+}
+
